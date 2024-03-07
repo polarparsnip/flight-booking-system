2284ecad3fb2be06fa9ca4248049955bafb8c8ca
polarparsnip <>
2024-03-07 18:25:14 +0000

    refactor sql stuff

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ path="flightBookingSystem.db"
 
 # execute SQLite commands
 if os.name == 'nt':
-    cmd = f'cmd /c "sqlite3 db/{path} < db/schema.sql && sqlite3 db/{path} < db/insert.sql"'
+    cmd = f'cmd /c "sqlite3 sql/{path} < sql/schema.sql && sqlite3 sql/{path} < sql/insert.sql"'
 
 else:
-    cmd = f"cd db && sqlite3 {path} < schema.sql && sqlite3 {path} < insert.sql"
+    cmd = f"cd sql && sqlite3 {path} < schema.sql && sqlite3 {path} < insert.sql"
 
 os.system(cmd)
